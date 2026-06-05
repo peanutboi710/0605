5ad98230a007cb5c0f3933e3e5dc1415088f7a44
peanutboi710 <figaro0928@gmail.com>
2026-06-05 11:10:28 +0900

    langchain

--- 02_langchain.js
+++ 02_langchain.js
@@ -1,19 +1,44 @@
+// 새로운 터미널을 하단에 (+)로 만들어서...
+// npm i @langchain/core
+// https://www.npmjs.com/package/@langchain/core
+
+// 환경변수 불러오기
 const dotenv = require("dotenv");
 dotenv.config();
 
+// 의존성
 const express = require("express");
-const PORT = process.env.PORT ?? 3000;
+const {} = require("@langchain/core");
+const { ChatGoogleGenerativeAI } = require("@langchain/google-genai");
 
+// 서버 세팅
+// const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT_01 ?? 3000;
 const app = express();
 
+// 미들웨어
 app.use(express.json());
 
+// 라우터, 엔드포인트 ...
 app.post("/chat", async (req, res) => {
-    console.log({...req.body});
-    res.json({...req.body});
+  console.log(req.body);
+  const { provider, model, ask } = req.body;
+  console.log(`프로바이더 : ${provider}`);
+  let result;
+  switch (provider) {
+    case "google-genai":
+      // npm i @langchain/google-genai
+      // https://www.npmjs.com/package/@langchain/google-genai
+      result = await useGoogleGenAI(model, ask);
+      break;
+
+    default:
+      break;
+  }
+  res.json(req.body);
 });
 
+// 리스너
 app.listen(PORT, () => {
-    console.log(`${PORT}에서 Listen 중`);
+  console.log(`${PORT}에서 Listen 중`);
 });
-
